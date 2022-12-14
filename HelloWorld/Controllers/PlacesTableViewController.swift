c56cc0179406f7887309fa26e1a1871e1d88bd68
Magdalena Struniawska <mag.sok@onet.pl>
2022-12-14 08:02:15 +0100

    first reposetry HelloWorld App

---
+++ HelloWorld/Controllers/PlacesTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PlacesTableViewController.swift
+//  HelloWorld
+//
+//  Created by Slawek on 13/12/2022.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+
+class PlacesTableViewController: UITableViewController {
+    
+    var userLocation: CLLocation
+    let places: [PlaceAnnotation]
+    
+    init(userLocation: CLLocation, places: [PlaceAnnotation]) {
+        self.userLocation = userLocation
+        self.places = places
+        super.init(nibName: nil, bundle: nil)
+        
+        // register cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PlaceCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        places.count
+    }
+    private func calculateDistance(from: CLLocation, to: CLLocation) -> CLLocationDistance {
+        from.distance(from: to)
+    }
+    private func formatDistanceForDisplay(_ distance: CLLocationDistance) -> String {
+        let meters = Measurement(value: distance, unit: UnitLength.meters)
+        return meters.converted(to: .miles).formatted()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell", for: indexPath)
+        let place = places[indexPath.row]
+        
+        //cell configuration
+        var content = cell.defaultContentConfiguration()
+        content.text = place.name
+        content.secondaryText = formatDistanceForDisplay(calculateDistance(from: userLocation, to: place.location))
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ HelloWorld/Extensions/CLLocation+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  CLLocation+Extensions.swift
+//  HelloWorld
+//
+//  Created by Slawek on 13/12/2022.
+//
+import CoreLocation
+import Foundation
+
+extension CLLocation {
+
+static var `default`: CLLocation {
+    CLLocation(latitude: 36.063457, longitude: -95.880516)
+    }
+}
